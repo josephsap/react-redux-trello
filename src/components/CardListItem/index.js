0599efb292afc36af255915cc2be35dedd9a8ca9
Joe Saperstein <joseph.saperstein@ihsmarkit.com>
2018-09-28 13:22:25 -0600

    table example

--- src/components/CardListItem/index.js
+++ src/components/CardListItem/index.js
@@ -4,6 +4,7 @@ import _ from 'lodash';
 
 const CardListItem = (props) => {
   const parentListItems = [];
+  let rows = [];
   const lists = props.children.data.map((listItem) => {
     parentListItems.push(listItem);
     return <li key={listItem.id} id={"list-" + listItem.id}>{listItem.listName}</li>
@@ -13,6 +14,24 @@ const CardListItem = (props) => {
   const cardsArr = props.cards.cards;
   const merged = [].concat.apply([], cardsArr);
   // console.log(parentListItems, '09')
+
+
+  //   render(){
+  //     let rows = [];
+  //     for (var i = 0; i < this.state.size; i++){
+  //       let rowID = `row${i}`
+  //       let cell = []
+  //       for (var idx = 0; idx < this.state.size; idx++){
+  //         let cellID = `cell${i}-${idx}`
+  //         cell.push(<td key={cellID} id={cellID}></td>)
+  //       }
+  //       rows.push(<tr key={i} id={rowID}>{cell}</tr>)
+  //     }
+
+
+
+
+
   const cards = merged.map((cardItem, index) => {
   console.log(parentListItems[index], "-----")
   // if(parentListItems[index].boardId) {
@@ -57,5 +76,37 @@ const CardListItem = (props) => {
   );
 
 };
+
+
+//   constructor(props){
+//     super(props);
+//     this.state = {size: 3}
+//   }
+//   render(){
+//     let rows = [];
+//     for (var i = 0; i < this.state.size; i++){
+//       let rowID = `row${i}`
+//       let cell = []
+//       for (var idx = 0; idx < this.state.size; idx++){
+//         let cellID = `cell${i}-${idx}`
+//         cell.push(<td key={cellID} id={cellID}></td>)
+//       }
+//       rows.push(<tr key={i} id={rowID}>{cell}</tr>)
+//     }
+//     return(
+//       <div className="container">
+//         <div className="row">
+//           <div className="col s12 board">
+//             <table id="simple-board">
+//                <tbody>
+//                  {rows}
+//                </tbody>
+//              </table>
+//           </div>
+//         </div>
+//       </div>
+//     )
+//   }
+// }
 
 export default CardListItem;
